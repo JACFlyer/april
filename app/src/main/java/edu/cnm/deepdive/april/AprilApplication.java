b01a1686e791c17a20caae999e938846ba4ed303
JACFlyer <58670895+JACFlyer@users.noreply.github.com>
2020-02-12 17:28:36 -0700

    AprilApplication completed

--- app/src/main/java/edu/cnm/deepdive/april/AprilApplication.java
+++ app/src/main/java/edu/cnm/deepdive/april/AprilApplication.java
@@ -2,7 +2,7 @@ package edu.cnm.deepdive.april;
 
 import android.app.Application;
 import com.facebook.stetho.Stetho;
-
+import edu.cnm.deepdive.april.service.AprilDatabase;
 public class AprilApplication extends Application {
 
 
@@ -10,5 +10,8 @@ public class AprilApplication extends Application {
   public void onCreate() {
     super.onCreate();
     Stetho.initializeWithDefaults(this);
+    AprilDatabase.setContext(this);
+    new Thread(() -> AprilDatabase.getInstance().getClientProfileDao().delete()).start();
+
   }
 }
---
+++ app/src/main/java/edu/cnm/deepdive/april/service/AprilDatabase.java
@@ -0,0 +1,76 @@
+package edu.cnm.deepdive.april.service;
+
+import android.app.Application;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.room.TypeConverter;
+import androidx.room.TypeConverters;
+import edu.cnm.deepdive.april.model.dao.AssessmentDao;
+import edu.cnm.deepdive.april.model.dao.ClientProfileDao;
+import edu.cnm.deepdive.april.model.entity.Assessment;
+import edu.cnm.deepdive.april.model.entity.ClientProfile;
+import edu.cnm.deepdive.april.service.AprilDatabase.Converters;
+import java.util.Date;
+
+
+
+  @Database(
+      entities = {ClientProfile.class, Assessment.class},
+      version = 1,
+      exportSchema = true
+  )
+  @TypeConverters({Converters.class})
+
+  public abstract class AprilDatabase extends RoomDatabase {
+
+    private static final String DB_NAME = "client_db";
+
+    private static Application context;
+
+    public static void setContext(Application context) {
+      AprilDatabase.context = context;
+    }
+
+    public static AprilDatabase getInstance() {
+      return InstanceHolder.INSTANCE;
+    }
+
+    public abstract ClientProfileDao getClientProfileDao();
+
+    public abstract AssessmentDao getAssessmentDao();
+
+    private static class InstanceHolder {
+
+      private static final AprilDatabase INSTANCE = Room.databaseBuilder(
+          context, AprilDatabase.class, DB_NAME)
+          .build();
+
+    }
+
+    public static class Converters {
+
+      @TypeConverter
+      public static Long fromDate(Date date) {
+        return (date != null) ? date.getTime() : null;
+      }
+
+      @TypeConverter
+      public static Date fromLong(Long value) {
+        return (value != null) ? new Date(value) : null;
+      }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
