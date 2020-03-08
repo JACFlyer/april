f8fdbd29ae200de1c3236d5911d4e38a5072fba0
JACFlyer <58670895+JACFlyer@users.noreply.github.com>
2020-03-08 11:32:06 -0600

    CRUD Operations completed in Daos.

--- app/src/main/java/edu/cnm/deepdive/april/model/dao/LaborDao.java
+++ app/src/main/java/edu/cnm/deepdive/april/model/dao/LaborDao.java
@@ -4,8 +4,10 @@ import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Update;
 import edu.cnm.deepdive.april.model.entity.Assessment;
 import edu.cnm.deepdive.april.model.entity.ClientProfile;
+import edu.cnm.deepdive.april.model.entity.FetalCondition;
 import edu.cnm.deepdive.april.model.entity.Labor;
 import io.reactivex.Single;
 import java.util.Collection;
@@ -15,6 +17,7 @@ import java.util.List;
 @Dao
 public interface LaborDao {
 
+  /** Establishing Create CRUD-Operation **/
   @Insert
   Single<Long> insert(Labor labor);
 
@@ -24,6 +27,30 @@ public interface LaborDao {
   @Insert
   Single<List<Long>> insert(Labor... labors);
 
+
+
+  /** Establishing Read CRUD-Operation **/
+  @Query("SELECT * FROM Labor ORDER BY labor_id")
+  List<FetalCondition> select();
+
+
+  @Query("SELECT * FROM Labor WHERE labor_id = :id")
+  Single<ClientProfile> select(long id);
+
+
+
+  /** Establishing Update CRUD-Operation **/
+  @Update
+  Single<Long> update(Labor labor);
+
+  @Update
+  Single<List<Long>> update(Collection<Labor> labors);
+
+  @Update
+  Single<List<Long>> update(Labor... labors);
+
+
+  /** Establishing Delete CRUD-operation **/
   @Delete
   Single<Integer> delete(Labor labor);
 
@@ -32,11 +59,5 @@ public interface LaborDao {
 
   @Delete
   Single<Integer> delete(Labor... labors);
-
-  @Query("SELECT * FROM Labor ORDER BY labor_id")
-  List<Labor> select();
-
-  @Query("SELECT * FROM Labor WHERE labor_id = :id")
-  Single<Labor> select(long id);
-
+  
 }
